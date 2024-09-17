edd6502e9b7088e2ed3916f9a25fd52946a36d6f
Konrad Zięba <162624@student.uwm.edu.pl>
2024-09-17 14:29:51 +0200

    add zod schemas and validate middleware

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { validate } from '../middlewares/validate.js';
+import { RestaurantSchema, type Restaurant } from '../schemas/restaurant.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', validate(RestaurantSchema), async (req, res) => {
+	const data = req.body as Restaurant;
 	res.send('All restaurants');
 });
 
